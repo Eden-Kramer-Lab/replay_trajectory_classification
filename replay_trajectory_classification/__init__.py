e2243b66d3a7f19fdf506312bc4b28a62e1ef2bd
Eric Denovellis <edeno@bu.edu>
2019-03-22 11:26:22 -0400

    Add data loading module for examples

--- replay_trajectory_classification/__init__.py
+++ replay_trajectory_classification/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 from .classifier import ClusterlessClassifier, SortedSpikesClassifier
 from .decoder import ClusterlessDecoder, SortedSpikesDecoder
+from .load_example_data import load_data
