1532d7ec3b81aa6208133a2d497b53d64183c200
Eric Denovellis <edeno@bu.edu>
2019-03-29 11:39:53 -0400

    Fix requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ['numpy >= 1.11', 'statsmodels', 'numba', 'matplotlib',
-                    'xarray', 'scipy', 'scikit-learn', 'regularized_glm',
-                    'dask', 'patsy', 'loren_frank_data_processing']
+INSTALL_REQUIRES = ['numpy', 'statsmodels', 'numba', 'matplotlib',
+                    "pandas >= 0.21.0" 'xarray', 'scipy', 'scikit-learn',
+                    'regularized_glm', 'dask', 'patsy',
+                    'loren_frank_data_processing']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
