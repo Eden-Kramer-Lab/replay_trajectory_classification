e6998b23d9c1d472fa464f5b8e0abd6e66fac3fb
Eric Denovellis <edeno@bu.edu>
2019-03-29 12:24:13 -0400

    Fix missing import

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages, setup
 
 INSTALL_REQUIRES = ['numpy', 'statsmodels', 'numba', 'matplotlib',
                     'pandas', 'xarray', 'scipy', 'scikit-learn',
-                    'regularized_glm', 'dask', 'patsy']
+                    'regularized_glm', 'dask', 'patsy', 'networkx']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
     name='replay_trajectory_classification',
-    version='0.2.0.dev0',
+    version='0.2.1.dev0',
     license='MIT',
     description=('Classify replay trajectories.'),
     author='Eric Denovellis',
