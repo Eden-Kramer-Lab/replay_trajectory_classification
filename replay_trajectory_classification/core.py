2bb89fde8d6a02456098f49cb9972a719ac27c96
Eric Denovellis <edeno@bu.edu>
2019-05-30 14:50:32 -0400

    Minor formatting

--- replay_trajectory_classification/core.py
+++ replay_trajectory_classification/core.py
@@ -125,7 +125,8 @@ def _causal_decode(initial_conditions, state_transition, likelihood):
     n_time = likelihood.shape[0]
     posterior = np.zeros_like(likelihood)
 
-    posterior[0] = initial_conditions.copy() * likelihood[0]
+    posterior[0] = normalize_to_probability(
+        initial_conditions.copy() * likelihood[0])
 
     for time_ind in np.arange(1, n_time):
         prior = state_transition @ posterior[time_ind - 1]
