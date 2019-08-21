240ba291263d3e927aca961d279aff9017331b67
Eric Denovellis <edeno@bu.edu>
2019-08-21 10:59:57 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
     name='replay_trajectory_classification',
-    version='0.5.13.dev0',
+    version='0.5.14.dev0',
     license='MIT',
     description=('Classify replay trajectories.'),
     author='Eric Denovellis',
