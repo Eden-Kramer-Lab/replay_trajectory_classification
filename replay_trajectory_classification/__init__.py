2f3b0810c7cf5e27b6ded1633f69e238b12110ad
Eric Denovellis <edeno@bu.edu>
2020-10-14 16:41:43 -0700

    Add track linearization code

--- replay_trajectory_classification/__init__.py
+++ replay_trajectory_classification/__init__.py
@@ -1,4 +1,9 @@
 # flake8: noqa
-from .bins import make_track_graph, plot_track_graph
-from .classifier import ClusterlessClassifier, SortedSpikesClassifier
-from .decoder import ClusterlessDecoder, SortedSpikesDecoder
+from replay_trajectory_classification.classifier import (
+    ClusterlessClassifier, SortedSpikesClassifier)
+from replay_trajectory_classification.decoder import (ClusterlessDecoder,
+                                                      SortedSpikesDecoder)
+from track_linearization import (get_linearized_position,
+                                 make_actual_vs_linearized_position_movie,
+                                 make_track_graph, plot_graph_as_1D,
+                                 plot_track_graph)
