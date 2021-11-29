6f3c1833f41d23f4b34eb312766aefd11a091a60
Eric Denovellis <edeno@bu.edu>
2021-11-29 12:04:52 -0500

    Add the ability for uniform conditions on only one environment

--- replay_trajectory_classification/initial_conditions.py
+++ replay_trajectory_classification/initial_conditions.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+import numpy as np
+
 
 @dataclass
 class UniformInitialConditions:
@@ -14,8 +16,32 @@ class UniformInitialConditions:
                 environments[environments.index(environment_name)]
                 .is_track_interior_
                 .ravel(order='F'))
-            n_place_bins = is_track_interior.sum()
-            n_total_place_bins += n_place_bins
+            n_total_place_bins += is_track_interior.sum()
             initial_conditions.append(is_track_interior)
+
+        return [ic / n_total_place_bins for ic in initial_conditions]
+
+
+@dataclass
+class UniformOneEnvironmentInitialConditions:
+    environment_name: str = ''
+
+    def make_initial_conditions(self,
+                                environments: tuple,
+                                environment_names_to_state: tuple):
+        n_total_place_bins = 0
+        initial_conditions = []
+
+        for environment_name in environment_names_to_state:
+            is_track_interior = (
+                environments[environments.index(environment_name)]
+                .is_track_interior_
+                .ravel(order='F'))
+
+            if self.environment_name == environment_name:
+                initial_conditions.append(is_track_interior)
+                n_total_place_bins += is_track_interior.sum()
+            else:
+                initial_conditions.append(np.zeros_like(is_track_interior))
 
         return [ic / n_total_place_bins for ic in initial_conditions]
