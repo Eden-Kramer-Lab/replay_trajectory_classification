07eeb2542cfd586c7bbe04c10d9bd6a321007029
Eric Denovellis <edeno@bu.edu>
2022-02-23 15:11:08 -0800

    Fix imports

--- replay_trajectory_classification/likelihoods/__init__.py
+++ replay_trajectory_classification/likelihoods/__init__.py
@@ -5,23 +5,11 @@ from .multiunit_likelihood import (estimate_multiunit_likelihood,
                                    fit_multiunit_likelihood)
 from .multiunit_likelihood_gpu import (estimate_multiunit_likelihood_gpu,
                                        fit_multiunit_likelihood_gpu)
-from .multiunit_likelihood_gpu_pinned_arrays import (
-    estimate_multiunit_likelihood_gpu_pinned_arrays,
-    fit_multiunit_likelihood_gpu_pinned_arrays)
-from .multiunit_likelihood_gpu_shared import (
-    estimate_multiunit_likelihood_gpu_shared,
-    fit_multiunit_likelihood_gpu_shared)
 from .multiunit_likelihood_integer import (
     estimate_multiunit_likelihood_integer, fit_multiunit_likelihood_integer)
 from .multiunit_likelihood_integer_gpu import (
     estimate_multiunit_likelihood_integer_gpu,
     fit_multiunit_likelihood_integer_gpu)
-from .multiunit_likelihood_integer_no_dask import (
-    estimate_multiunit_likelihood_integer_no_dask,
-    fit_multiunit_likelihood_integer_no_dask)
-from .multiunit_likelihood_integer_pass_position import (
-    estimate_multiunit_likelihood_integer_pass_position,
-    fit_multiunit_likelihood_integer_pass_position)
 from .spiking_likelihood import (estimate_place_fields,
                                  estimate_spiking_likelihood)
 
