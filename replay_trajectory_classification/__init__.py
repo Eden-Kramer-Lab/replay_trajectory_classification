5f7683acb4069b898a3ca260c7411e885cc0159c
Eric Denovellis <edeno@bu.edu>
2022-04-10 10:28:09 -0700

    Add relevant classes to top level

--- replay_trajectory_classification/__init__.py
+++ replay_trajectory_classification/__init__.py
@@ -1,8 +1,17 @@
 # flake8: noqa
 from replay_trajectory_classification.classifier import (
     ClusterlessClassifier, SortedSpikesClassifier)
+from replay_trajectory_classification.continuous_state_transitions import (
+    EmpiricalMovement, Identity, RandomWalk, RandomWalkDirection1,
+    RandomWalkDirection2, Uniform)
 from replay_trajectory_classification.decoder import (ClusterlessDecoder,
                                                       SortedSpikesDecoder)
+from replay_trajectory_classification.discrete_state_transitions import (
+    DiagonalDiscrete, RandomDiscrete, UniformDiscrete)
+from replay_trajectory_classification.environments import Environment
+from replay_trajectory_classification.initial_conditions import \
+    UniformInitialConditions
+from replay_trajectory_classification.observation_model import ObservationModel
 from track_linearization import (get_linearized_position,
                                  make_actual_vs_linearized_position_movie,
                                  make_track_graph, plot_graph_as_1D,
