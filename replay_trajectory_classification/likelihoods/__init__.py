614c0034f2eb2a865b577b2efa9d26d1b249e13d
Eric Denovellis <edeno@bu.edu>
2022-05-13 14:33:05 -0700

    Remove unused algorithm

--- replay_trajectory_classification/likelihoods/__init__.py
+++ replay_trajectory_classification/likelihoods/__init__.py
@@ -3,6 +3,8 @@ from .calcium_likelihood import (estimate_calcium_likelihood,
                                  estimate_calcium_place_fields)
 from .multiunit_likelihood import (estimate_multiunit_likelihood,
                                    fit_multiunit_likelihood)
+from .multiunit_likelihood2 import (estimate_multiunit_likelihood2,
+                                    fit_multiunit_likelihood2)
 from .multiunit_likelihood_gpu import (estimate_multiunit_likelihood_gpu,
                                        fit_multiunit_likelihood_gpu)
 from .multiunit_likelihood_gpu2 import (estimate_multiunit_likelihood_gpu2,
@@ -15,9 +17,6 @@ from .multiunit_likelihood_integer_gpu import (
 from .multiunit_likelihood_integer_gpu2 import (
     estimate_multiunit_likelihood_integer_gpu2,
     fit_multiunit_likelihood_integer_gpu2)
-from .multiunit_likelihood_integer_gpu3 import (
-    estimate_multiunit_likelihood_integer_gpu3,
-    fit_multiunit_likelihood_integer_gpu3)
 from .spiking_likelihood_glm import (estimate_place_fields,
                                      estimate_spiking_likelihood)
 from .spiking_likelihood_kde import (estimate_place_fields_kde,
@@ -27,6 +26,9 @@ _ClUSTERLESS_ALGORITHMS = {
     'multiunit_likelihood': (
         fit_multiunit_likelihood,
         estimate_multiunit_likelihood),
+    'multiunit_likelihood2': (
+        fit_multiunit_likelihood2,
+        estimate_multiunit_likelihood2),
     'multiunit_likelihood_integer': (
         fit_multiunit_likelihood_integer,
         estimate_multiunit_likelihood_integer),
@@ -36,9 +38,6 @@ _ClUSTERLESS_ALGORITHMS = {
     'multiunit_likelihood_integer_gpu2': (
         fit_multiunit_likelihood_integer_gpu2,
         estimate_multiunit_likelihood_integer_gpu2),
-    'multiunit_likelihood_integer_gpu3': (
-        fit_multiunit_likelihood_integer_gpu3,
-        estimate_multiunit_likelihood_integer_gpu3),
     'multiunit_likelihood_gpu': (
         fit_multiunit_likelihood_gpu,
         estimate_multiunit_likelihood_gpu),
