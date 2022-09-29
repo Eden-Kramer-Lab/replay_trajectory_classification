ad6494a3615358e63b1a54bd9b2ecb71fc03bc6b
Eric Denovellis <edeno@bu.edu>
2022-09-29 15:48:57 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -2,20 +2,35 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ['numpy', 'statsmodels', 'numba', 'matplotlib',
-                    'pandas', 'xarray', 'scipy', 'scikit-learn', 'scikit-image',
-                    'regularized_glm', 'dask', 'patsy', 'networkx', 'joblib',
-                    'track_linearization', 'tqdm', 'seaborn']
-TESTS_REQUIRE = ['pytest >= 2.7.1']
+INSTALL_REQUIRES = [
+    "numpy",
+    "statsmodels",
+    "numba",
+    "matplotlib",
+    "pandas",
+    "xarray",
+    "scipy",
+    "scikit-learn",
+    "scikit-image",
+    "regularized_glm",
+    "dask",
+    "patsy",
+    "networkx",
+    "joblib",
+    "track_linearization",
+    "tqdm",
+    "seaborn",
+]
+TESTS_REQUIRE = ["pytest >= 2.7.1"]
 
 setup(
-    name='replay_trajectory_classification',
-    version='1.3.4',
-    license='MIT',
-    description=('Classify replay trajectories.'),
-    author='Eric Denovellis',
-    author_email='eric.denovellis@ucsf.edu',
-    url='https://github.com/Eden-Kramer-Lab/replay_trajectory_classification',
+    name="replay_trajectory_classification",
+    version="1.3.5",
+    license="MIT",
+    description=("Classify replay trajectories."),
+    author="Eric Denovellis",
+    author_email="eric.denovellis@ucsf.edu",
+    url="https://github.com/Eden-Kramer-Lab/replay_trajectory_classification",
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     tests_require=TESTS_REQUIRE,
