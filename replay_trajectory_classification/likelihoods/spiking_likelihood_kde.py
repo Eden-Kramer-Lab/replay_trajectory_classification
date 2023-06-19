03aefd9f769cd4bcfcd8a941caefc4c353ac25c9
Eric Denovellis <edeno@bu.edu>
2023-06-19 14:45:19 -0700

    Fix types

--- replay_trajectory_classification/likelihoods/spiking_likelihood_kde.py
+++ replay_trajectory_classification/likelihoods/spiking_likelihood_kde.py
@@ -321,7 +321,7 @@ def estimate_spiking_likelihood_kde(
         is_track_interior = is_track_interior.ravel(order="F")
     else:
         n_bins = conditional_intensity.shape[0]
-        is_track_interior = np.ones((n_bins,), dtype=np.bool)
+        is_track_interior = np.ones((n_bins,), dtype=bool)
 
     log_likelihood = combined_likelihood(spikes, conditional_intensity)
 
